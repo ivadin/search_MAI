f783af1d755bb03b66ef827a88b67017e700c50a
Ivan Demin <ivadin@rambler.ru>
2019-04-16 23:46:22 +0300

    made 5th lab

--- l5_index/l5.py
+++ l5_index/l5.py
@@ -4,9 +4,12 @@ import json
 from os import walk
 from datetime import datetime
 from collections import defaultdict
+import hashlib
 
-FORMAT_TO_BINARY = 'q'
-SIZE_OF_ONE_ELEM = 8
+FORMAT_TO_LL = 'q'
+FORMAT_TO_CHAR = 's'
+SIZE_OF_LL = 8
+SIZE_OF_CHAR = 1
 URL_PREFFIX = 'https://ru.wikipedia.org/wiki/'
 
 
@@ -35,24 +38,25 @@ def load_obj(name):
         return pickle.load(f)
 
 
+@timer
 def save_obj(obj, name):
     with open(name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 
 def write_n_digits_to_binary_doc_id(list_of_digits, file_name):
-    with open(file_name, 'wb') as f:
+    with open(file_name, 'ab') as f:
         n = len(list_of_digits)
-        frm = str(n) + FORMAT_TO_BINARY
+        frm = str(n) + FORMAT_TO_LL
         value_to_write = struct.pack(frm, *list_of_digits)
         f.write(value_to_write)
 
 
 def read_form_binary_doc_id(offset, file_name, pos_in_file=0):
     with open(file_name, 'rb') as f:
-        f.seek(pos_in_file * SIZE_OF_ONE_ELEM)
-        values = f.read(offset*SIZE_OF_ONE_ELEM)
-        frm = str(offset) + FORMAT_TO_BINARY
+        f.seek(pos_in_file * SIZE_OF_LL)
+        values = f.read(offset*SIZE_OF_LL)
+        frm = str(offset) + FORMAT_TO_LL
         return struct.unpack(frm, values)
 
 
@@ -67,12 +71,25 @@ def create_doc_id_files():
         else:
             doc_id[title] = index
             index += 1
+    expect_size = 0
+    with open('doc_id', 'ab') as f:
+        for title, id in doc_id.items():
+            # frm = id + len(title) + len(title) sizeof(char)
+            offset = len(title)
+            expect_size += SIZE_OF_LL + SIZE_OF_LL + offset * SIZE_OF_CHAR
+            frm = FORMAT_TO_LL + FORMAT_TO_LL + str(offset) + FORMAT_TO_CHAR
+            f.write(struct.pack(frm, id, offset, title.encode('utf-8')))
+    print("expected_size: %s" % expect_size)
     return doc_id
 
 
+def hash_str(s):
+    return int(hashlib.sha1(s.encode('utf-8')).hexdigest(), 16) % (10 ** 8)
+
+
 @timer
-def create_raw_index(doc_id):
-    raw_index = defaultdict(list)
+def create_raw_invert_index(doc_id):
+    raw_invert_index = defaultdict(list)
     dir_name = 'data_url_tokens'
     for token in get_articles_name(dn=dir_name):
         with open("../" + dir_name + "/" + token, 'r') as f:
@@ -80,9 +97,26 @@ def create_raw_index(doc_id):
             title = token[:-4]
             my_id = doc_id[title]
             for t in tokens_list:
-                raw_index[t].append(my_id)
-    return raw_index
+                word_hash = hash_str(t)
+                raw_invert_index[word_hash].append(my_id)
+
+    invert_index = dict(raw_invert_index)
+
+    # offset в файле считаем как количество записаных чисел, а не байт, так как при считывании нужно передавать
+    # количество чисел(функция read_form_binary_doc_id)
+    offset_in_file = 0
+    for key, value in raw_invert_index.items():
+        k = len(value)
+        offset_for_word = offset_in_file
+        offset_in_file += k
+        invert_index[key] = (offset_for_word, k)
+
+        write_n_digits_to_binary_doc_id(value, 'cord_blocks')
+
+    print("expected_size: %s" % (offset_in_file * SIZE_OF_LL))
+    return invert_index
 
 
 DOC_ID = create_doc_id_files()
-RAW_INDEX = create_raw_index(DOC_ID)
+INVERT_INDEX = create_raw_invert_index(DOC_ID)
+save_obj(INVERT_INDEX, name='INVERT_INDEX')
